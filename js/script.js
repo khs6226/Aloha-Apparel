fd6a931343727bffb6dccfcc4283864b6b8303a2
khs6226 <khsun6226@naver.com>
2019-04-30 03:25:44 -0700

    added email verification

--- js/script.js
+++ js/script.js
@@ -14,8 +14,39 @@ document.addEventListener("DOMContentLoaded", function() {
      // options
      cellAlign: 'left',
      contain: true,
-     prevNextButtons: false
+     prevNextButtons: false,
+     autoPlay: 2000
     });
+
+    let submit = document.getElementById('submit');
+    submit.addEventListener('click', /* function ValidateEmail(mail) {
+     if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(subscribeForm.email.value))
+      {
+        alert("Thank you for subscribing!");
+        return (true);
+      }
+    else {
+       alert("Please enter a valid email address!");
+       return (false);
+       }
+    } */
+    
+    
+    
+    function validateForm(s) {
+        s.preventDefault();
+        let x = document.getElementById('email');
+        if(x.value === "") {
+            alert('Please enter your email address');
+            x.focus();
+            return false;
+        }
+        else {
+        alert('Thank you for subscribing!');
+        return true;
+        };
+    });
+    
 });
             
 
